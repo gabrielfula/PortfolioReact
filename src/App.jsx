124d18a6be35b5209263d050e5e65f797d5c1bf3
gabrielfula <gabriel.fulaneto0301@gmail.com>
2024-01-04 16:06:39 -0300

    refactor: delete div container

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,22 @@ import Tecnologia from "./components/Tecnologias/Tecnologia";
 import Sobre from "./components/Sobre/Sobre";
 import Projetos from "./components/Projetos/Projetos";
 import Footer from "./components/Footer/Footer";
-import { Container } from "./styles/Styled";
 
 export default function App() {
   return (
     <>
-      <Container>
-        <section>
-          <Navbar />
-          <Inicio />
-        </section>
-        <section>
-          <Sobre />
-        </section>
-        <section>
-          <Projetos />
-        </section>
-        <Container>
-          <Tecnologia />
-        </Container>
-        <Footer />
-      </Container>
+      <section>
+        <Navbar />
+        <Inicio />
+      </section>
+      <section>
+        <Sobre />
+      </section>
+      <section>
+        <Projetos />
+      </section>
+      <Tecnologia />
+      <Footer />
     </>
   );
 }
