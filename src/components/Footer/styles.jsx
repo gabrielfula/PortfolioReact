b2aeb8322d07866ec06d760b99e72844608977ce
gabrielfula <gabriel.fulaneto0301@gmail.com>
2024-01-04 17:01:49 -0300

    style: change padding footer

--- src/components/Footer/styles.jsx
+++ src/components/Footer/styles.jsx
@@ -3,12 +3,12 @@ import styled from "styled-components";
 export const ContentFooter = styled.section`
   height: 20vh;
   display: flex;
-  padding: 4rem;
+  padding: 4rem 4rem;
   justify-content: center;
   align-items: center;
   gap: 5rem;
 
   @media (max-width: 992px) {
-    padding: 30px;
+    padding-top: 5rem;
   }
 `;
